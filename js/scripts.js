50f4aaa3d6cbb4c1dc2608f5178875c5c5ed7211
Eduard-Condurache <eduardcondurache96@gmail.com>
2024-07-22 18:17:24 +0200

    Fatto il bonus.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,33 @@ const gridContainer = document.getElementById('grid-container');
 
 const cellsGenerator = document.getElementById('cells-btn-generator');
 
+const difficultySelect = document.getElementById('difficulty');
+
 
 cellsGenerator.addEventListener('click', function() {
     gridContainer.innerHTML = '';
+
+    const difText = difficultySelect.options[difficultySelect.selectedIndex].text;
+    let cellNumber;
+
+    gridContainer.classList.remove('easy-dif', 'normal-dif', 'hard-dif');
+
+    if (difText === 'Easy') {
+        cellNumber = 49;
+        gridContainer.classList.add('easy-dif');
+    }
+
+    else if (difText === 'Normal') {
+        cellNumber = 81;
+        gridContainer.classList.add('normal-dif');
+    }
+
+    else if (difText === 'Hard') {
+        cellNumber = 100;
+        gridContainer.classList.add('hard-dif');
+    }
     
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < cellNumber; i++) {
         const cell = document.createElement('div');
         cell.className = 'cell';
 
@@ -25,6 +47,7 @@ cellsGenerator.addEventListener('click', function() {
         })
     }
 })
+
     
 
 
